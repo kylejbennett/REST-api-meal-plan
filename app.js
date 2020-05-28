a4a5166b6fcd62680f6fc9b5fe5bcd2c88b1a633
Kyle Bennett <kyle.j.bennett5@gmail.com>
2020-05-28 14:26:33 -0400

    Database cloud connection and dotenv

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-app.js
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+require('dotenv/config');
+
+// Connect to Mongoose
+mongoose.connect(
+	process.env.DB_CONNECTION,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	() => console.log('Connected to DB')
+);
+
+var db = mongoose.connection;
+
+app.get('/', function(req, res){
+	res.send('Hello World!')
+});
+
+app.listen(5000);
+console.log('Running on Port 5000!');
